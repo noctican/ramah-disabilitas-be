a529821703984029e49e2b3c8d9d11395a01de30
mohamadarif03 <mohamadarif5392@gmail.com>
2026-01-08 06:35:43 +0700

    test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
 	"ramah-disabilitas-be/internal/router"
 	"ramah-disabilitas-be/pkg/database"
 
@@ -15,21 +14,13 @@ func main() {
 		log.Println("Warning: No .env file found")
 	}
 
-	// 1. Ambil port dari Environment Variable yang diberikan Koyeb
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Default jika jalan di lokal
-	}
-
 	database.Connect()
 	database.Migrate()
 	database.SeedAdmin()
 
 	r := router.SetupRouter()
 
-	// 2. WAJIB: Bind ke 0.0.0.0, bukan localhost
-	log.Println("Server jalan di port:", port)
-	if err := r.Run("0.0.0.0:" + port); err != nil {
+	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
 }
